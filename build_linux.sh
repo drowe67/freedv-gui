2bfaee49dc626a354741963037a2c9f03543607f
David <david@rowetel.com>
2019-08-11 06:22:41 +0930

    build with codec2 development branch

--- build_linux.sh
+++ build_linux.sh
@@ -8,10 +8,13 @@ export FREEDVGUIDIR=${PWD}
 export CODEC2DIR=$FREEDVGUIDIR/codec2
 export LPCNETDIR=$FREEDVGUIDIR/LPCNet
 
+# change this when working on combined codec2/freedv-gui changes
+CODEC2_BRANCH=ss-phaselock
+
 # First build and install vanilla codec2 as we need -lcodec2 to build LPCNet
 cd $FREEDVGUIDIR
 git clone https://github.com/drowe67/codec2.git
-cd codec2 && git checkout master && git pull
+cd codec2 && git checkout $CODEC2_BRANCH && git pull
 mkdir -p build_linux && cd build_linux && rm -Rf * && cmake .. && make
 
 # OK, build and test LPCNet
