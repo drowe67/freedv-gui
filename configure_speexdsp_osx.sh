126c3879daa7c25f7b7e139cb2892d984955246b
Mooneer Salem <mooneer@gmail.com>
2020-11-15 21:35:20 -0800

    Add missed BUILD_OSX_UNIVERSAL checks.

--- configure_speexdsp_osx.sh
+++ configure_speexdsp_osx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CFLAGS="-g -O2 -arch arm64 -arch x86_64 -mmacosx-version-min=10.9" ./configure --prefix=$1 --disable-examples
+CFLAGS="-g -O2 -mmacosx-version-min=10.9" ./configure --prefix=$1 --disable-examples
