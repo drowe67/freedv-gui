379ae5b108efa7bd8de44bfaff4dbe3e6502f2a4
Mooneer Salem <mooneer@gmail.com>
2020-12-26 21:40:00 -0800

    Add file headers.

--- src/callsign_encoder.cpp
+++ src/callsign_encoder.cpp
@@ -1,3 +1,25 @@
+//==========================================================================
+// Name:            callsign_decoder.cpp
+//
+// Purpose:         Encodes and decodes received callsigns.
+// Created:         December 26, 2020
+// Authors:         Mooneer Salem
+//
+// License:
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License version 2.1,
+//  as published by the Free Software Foundation.  This program is
+//  distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+//  License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+//==========================================================================
+
 #include <cstdlib>
 #include <cstring>
 #include "callsign_encoder.h"
