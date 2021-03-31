18b0cbc0eebbb17296d67edd504ed0b24782bf37
Mooneer Salem <mooneer@gmail.com>
2021-03-31 01:39:03 -0700

    Improvements to sync reporting when auto RX mode is disabled.

--- src/freedv_interface.h
+++ src/freedv_interface.h
@@ -1,3 +1,24 @@
+//==========================================================================
+// Name:            freedv_interface.h
+// Purpose:         Implements a wrapper around the Codec2 FreeDV interface.
+// Created:         March 31, 2021
+// Authors:         Mooneer Salem
+// 
+// License:
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.1,
+//  as published by the Free Software Foundation.  This program is
+//  distributed in the hope that it will be useful, but WITHOUT ANY
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+//  License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+//==========================================================================
+
 #ifndef CODEC2_INTERFACE_H
 #define CODEC2_INTERFACE_H
 
