c8da94dff8338b6a08dad797d4411d917c01b564
Mooneer Salem <mooneer@gmail.com>
2021-09-01 19:17:45 -0700

    Switch branches back to master in build scripts prior to merge.

--- build_windows.sh
+++ build_windows.sh
@@ -21,7 +21,7 @@ export LPCNETDIR=$FREEDVGUIDIR/LPCNet
 # First build and install vanilla codec2 as we need -lcodec2 to build LPCNet
 cd $FREEDVGUIDIR
 git clone https://github.com/drowe67/codec2.git
-cd codec2 && git checkout ms-reliable-text && git pull
+cd codec2 && git checkout master && git pull
 mkdir -p $BUILD_DIR && cd $BUILD_DIR && rm -Rf *
 $CMAKE .. && make
 
