45d3193a2a3791a15bc2f78574588c4c0ca03348
Mooneer Salem <mooneer@gmail.com>
2021-12-19 01:16:14 -0800

    Add callback handlers for events the main code will need to handle.

--- src/audio/IAudioDevice.h
+++ src/audio/IAudioDevice.h
@@ -30,8 +30,42 @@
 class IAudioDevice
 {
 public:
+    typedef std::function<void(IAudioDevice&, void*, size_t, void*)> AudioDataCallbackFn;
+    typedef std::function<void(IAudioDevice&, void*)> AudioUnderflowCallbackFn;
+    typedef std::function<void(IAudioDevice&, void*)> AudioOverflowCallbackFn;
+    
     virtual void start() = 0;
     virtual void stop() = 0;
+    
+    // Set RX/TX ready callback.
+    // Callback must take the following parameters:
+    //    1. Audio device.
+    //    2. Pointer to buffer containing RX/TX data.
+    //    3. Size of buffer.
+    //    4. Pointer to user-provided state object (typically onAudioDataState, defined below).
+    void setOnAudioData(AudioDataCallbackFn fn, void* state);
+    
+    // Set overflow callback.
+    // Callback must take the following parameters:
+    //    1. Audio device.
+    //    2. Pointer to user-provided state object (typically onAudioOverflowState, defined below).
+    void setOnAudioOverflow(AudioOverflowCallbackFn fn, void* state);
+
+    // Set underflow callback.
+    // Callback must take the following parameters:
+    //    1. Audio device.
+    //    2. Pointer to user-provided state object (typically onAudioUnderflowState, defined below).
+    void setOnAudioUnderflow(AudioOverflowCallbackFn fn, void* state);
+    
+protected:
+    AudioDataCallbackFn onAudioDataFunction;
+    void* onAudioDataState;
+    
+    AudioOverflowCallbackFn onAudioOverflowFunction;
+    void* onAudioOverflowState;
+    
+    AudioUnderflowCallbackFn onAudioUnderflowFunction;
+    void* onAudioUnderflowState;
 };
 
-#endif // AUDIO_ENGINE_H
+#endif // I_AUDIO_DEVICE_H
