ecbb0c140e7c0d715c0342d7d214c2f9163b92b1
Mooneer Salem <mooneer@gmail.com>
2022-04-24 22:23:59 -0700

    Implement ResampleTest.

---
+++ src/pipeline/test/PipelineTestCommon.h
@@ -0,0 +1,43 @@
+#ifndef PIPELINE_TEST_COMMON_H
+#define PIPELINE_TEST_COMMON_H
+
+// Suite of common functions used by unit tests.
+
+#include <iostream>
+#include <functional>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <assert.h>
+
+inline short* generateOneSecondSineWave(float frequency, int sampleRate)
+{
+    short* result = new short[sampleRate];
+    assert(result != nullptr);
+    
+    for (auto n = 0; n < sampleRate; n++)
+    {
+        // f(n) = A * cos(2*pi*f*t) = A * cos(2*pi*f*n/sampleRate)
+        result[n] = frequency * cos(6.2832*(n)*400.0/sampleRate);
+    }
+    
+    return result;
+}
+
+inline void executeTestCase(std::string testName, std::function<bool()> fn)
+{
+    std::cout << "Executing " << testName << "...";
+    if (fn())
+    {
+        std::cout << "passed" << std::endl;
+    }
+    else
+    {
+        std::cout << "FAILED" << std::endl;
+        exit(-1);
+    }
+}
+
+#define TEST_CASE(name) executeTestCase( #name, name)
+
+#endif // PIPELINE_TEST_COMMON_H
