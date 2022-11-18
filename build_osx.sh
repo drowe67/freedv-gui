8454476baaaa55735e5f20d77794651ac62d9056
Mooneer Salem <mooneer@gmail.com>
2022-11-17 22:27:58 -0800

    Needed changes have been merged to master.

--- build_osx.sh
+++ build_osx.sh
@@ -8,7 +8,7 @@ export FREEDVGUIDIR=${PWD}
 export CODEC2DIR=$FREEDVGUIDIR/codec2
 export LPCNETDIR=$FREEDVGUIDIR/LPCNet
 export HAMLIBDIR=$FREEDVGUIDIR/hamlib
-export CODEC2_BRANCH=ms-macos-older-releases
+export CODEC2_BRANCH=master
 export LPCNET_BRANCH=master
 export UT_ENABLE=${UT_ENABLE:-0}
 
