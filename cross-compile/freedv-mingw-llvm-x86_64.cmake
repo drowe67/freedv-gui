54d390bf767e4bcc3a39d7d2e68bceb38098630e
Mooneer Salem <mooneer@gmail.com>
2022-11-30 22:21:57 -0800

    We can now build Windows executables with LLVM, but no installers yet.

---
+++ cross-compile/freedv-mingw-llvm-x86_64.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_SYSTEM_NAME Windows-MinGW)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(triple ${CMAKE_SYSTEM_PROCESSOR}-w64-mingw32)
+
+set(CMAKE_C_COMPILER ${triple}-clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER ${triple}-clang++)
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+set(CMAKE_RC_COMPILER ${triple}-windres)
+
+# For make package use.
+set(CMAKE_OBJDUMP ${triple}-objdump)
+
+# here is the target environment located
+#SET(CMAKE_FIND_ROOT_PATH ${MPATH})
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
