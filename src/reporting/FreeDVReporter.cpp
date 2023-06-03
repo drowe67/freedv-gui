007f4f476fc76ace371b083d436b9396720ddda2
Mooneer Salem <mooneer@gmail.com>
2023-06-03 00:53:52 -0700

    Implement race condition workaround per comments in sioclient-cpp GitHub issues.

--- src/reporting/FreeDVReporter.cpp
+++ src/reporting/FreeDVReporter.cpp
@@ -24,6 +24,7 @@
 
 FreeDVReporter::FreeDVReporter(std::string hostname, std::string callsign, std::string gridSquare, std::string software)
     : isExiting_(false)
+    , isConnecting_(false)
     , hostname_(hostname)
     , callsign_(callsign)
     , gridSquare_(gridSquare)
@@ -39,14 +40,28 @@ FreeDVReporter::~FreeDVReporter()
     isExiting_ = true;
     fnQueueConditionVariable_.notify_one();
     fnQueueThread_.join();
+    
+    // Workaround for race condition in sioclient
+    while (isConnecting_)
+    {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(20ms);
+    }
 }
 
 void FreeDVReporter::inAnalogMode(bool inAnalog)
 {
     std::unique_lock<std::mutex> lk(fnQueueMutex_);
-    fnQueue_.push_back([&]() {
+    fnQueue_.push_back([&, inAnalog]() {
         if (inAnalog)
         {
+            // Workaround for race condition in sioclient
+            while (isConnecting_)
+            {
+                using namespace std::chrono_literals;
+                std::this_thread::sleep_for(20ms);
+            }
+            
             sioClient_.sync_close();
         }
         else
@@ -60,39 +75,21 @@ void FreeDVReporter::inAnalogMode(bool inAnalog)
 void FreeDVReporter::freqChange(uint64_t frequency)
 {
     std::unique_lock<std::mutex> lk(fnQueueMutex_);
-    fnQueue_.push_back([&]() {
-        sio::message::ptr freqDataPtr = sio::object_message::create();
-        auto freqData = (sio::object_message*)freqDataPtr.get();
-        freqData->insert("freq", sio::int_message::create(frequency));
-        sioClient_.socket()->emit("freq_change", freqDataPtr);
-    
-        // Save last frequency in case we need to reconnect.
-        lastFrequency_ = frequency;
-    });
+    fnQueue_.push_back(std::bind(&FreeDVReporter::freqChangeImpl_, this, frequency));
     fnQueueConditionVariable_.notify_one();
 }
 
 void FreeDVReporter::transmit(std::string mode, bool tx)
 {
     std::unique_lock<std::mutex> lk(fnQueueMutex_);
-    fnQueue_.push_back([&]() {
-        sio::message::ptr txDataPtr = sio::object_message::create();
-        auto txData = (sio::object_message*)txDataPtr.get();
-        txData->insert("mode", mode);
-        txData->insert("transmitting", sio::bool_message::create(tx));
-        sioClient_.socket()->emit("tx_report", txDataPtr);
-    
-        // Save last mode and TX state in case we have to reconnect.
-        mode_ = mode;
-        tx_ = tx;
-    });
+    fnQueue_.push_back(std::bind(&FreeDVReporter::transmitImpl_, this, mode, tx));
     fnQueueConditionVariable_.notify_one();
 }
     
 void FreeDVReporter::addReceiveRecord(std::string callsign, std::string mode, uint64_t frequency, char snr)
 {
     std::unique_lock<std::mutex> lk(fnQueueMutex_);
-    fnQueue_.push_back([&]() {
+    fnQueue_.push_back([&, mode, snr]() {
         sio::message::ptr rxDataPtr = sio::object_message::create();
         auto rxData = (sio::object_message*)rxDataPtr.get();
     
@@ -124,16 +121,27 @@ void FreeDVReporter::connect_()
     // doesn't appear.
     sioClient_.set_socket_open_listener([&](std::string)
     {
-        freqChange(lastFrequency_);
-        transmit(mode_, tx_);
+        isConnecting_ = false;
+        freqChangeImpl_(lastFrequency_);
+        transmitImpl_(mode_, tx_);
     });
     
     sioClient_.set_reconnect_listener([&](unsigned, unsigned)
     {
-        freqChange(lastFrequency_);
-        transmit(mode_, tx_);
+        isConnecting_ = false;
+        freqChangeImpl_(lastFrequency_);
+        transmitImpl_(mode_, tx_);
     });
     
+    sioClient_.set_fail_listener([&]() {
+        isConnecting_ = false;
+    });
+    
+    sioClient_.set_close_listener([&](sio::client::close_reason const&) {
+        isConnecting_ = false;
+    });
+    
+    isConnecting_ = true;
     sioClient_.connect(std::string("http://") + hostname_ + "/", authPtr);
 }
 
@@ -149,8 +157,36 @@ void FreeDVReporter::threadEntryPoint_()
         // Execute queued method
         while (!isExiting_ && fnQueue_.size() > 0)
         {
-            fnQueue_.front()();
+            auto fn = fnQueue_.front();
             fnQueue_.erase(fnQueue_.begin());
+            
+            lk.unlock();
+            fn();
+            lk.lock();
         }
     }
+}
+
+void FreeDVReporter::freqChangeImpl_(uint64_t frequency)
+{
+    sio::message::ptr freqDataPtr = sio::object_message::create();
+    auto freqData = (sio::object_message*)freqDataPtr.get();
+    freqData->insert("freq", sio::int_message::create(frequency));
+    sioClient_.socket()->emit("freq_change", freqDataPtr);
+
+    // Save last frequency in case we need to reconnect.
+    lastFrequency_ = frequency;
+}
+
+void FreeDVReporter::transmitImpl_(std::string mode, bool tx)
+{
+    sio::message::ptr txDataPtr = sio::object_message::create();
+    auto txData = (sio::object_message*)txDataPtr.get();
+    txData->insert("mode", mode);
+    txData->insert("transmitting", sio::bool_message::create(tx));
+    sioClient_.socket()->emit("tx_report", txDataPtr);
+
+    // Save last mode and TX state in case we have to reconnect.
+    mode_ = mode;
+    tx_ = tx;
 }
