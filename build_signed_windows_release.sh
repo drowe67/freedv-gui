8bfc0b97aff4c0651e6ac93f2c2e8e35049cfe72
Mooneer Salem <mooneer@gmail.com>
2023-08-12 14:36:55 -0700

    Fix some bugs with the release script.

--- build_signed_windows_release.sh
+++ build_signed_windows_release.sh
@@ -17,13 +17,14 @@ fi
 
 CERT_URL_FILE=$1
 KEY_URL_FILE=$2
+CUR_DIR=$PWD
 
-cd $SCRIPT_DIR
-mkdir build_windows
-cd build_windows
+WIN_BUILD_DIR=$SCRIPT_DIR/build_windows
+mkdir $WIN_BUILD_DIR
+cd $WIN_BUILD_DIR
 
 for arch in x86_64 i686 armv7 aarch64; do
-    BUILD_ARCH_DIR="${SCRIPT_DIR}/build_win_$arch"
+    BUILD_ARCH_DIR="$WIN_BUILD_DIR/build_win_$arch"
 
     # Clear existing build
     rm -rf $BUILD_ARCH_DIR
@@ -33,8 +34,9 @@ for arch in x86_64 i686 armv7 aarch64; do
     # Kick off new build with the given architecture
     cmake -DSIGN_WINDOWS_BINARIES=1 -DPKCS11_CERTIFICATE_FILE=$CERT_URL_FILE -DPKCS11_KEY_FILE=$KEY_URL_FILE -DCMAKE_TOOLCHAIN_FILE=$SCRIPT_DIR/cross-compile/freedv-mingw-llvm-$arch.cmake $SCRIPT_DIR
     make -j6 package
-    cp FreeDV-*.exe ..
-    cd ..
+    cp FreeDV-*.exe $WIN_BUILD_DIR
+    cd $WIN_BUILD_DIR
 done
 
+cd $CUR_DIR
 echo "All Windows builds complete."
