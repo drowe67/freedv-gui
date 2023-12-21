98a6e97cc823ce6db2bb275e2d5663bb22803fc6
Mooneer Salem <mooneer@gmail.com>
2023-12-20 22:42:32 -0800

    Revert status message to 175px width.

--- src/freedv_reporter.cpp
+++ src/freedv_reporter.cpp
@@ -149,7 +149,7 @@ FreeDVReporterDialog::FreeDVReporterDialog(wxWindow* parent, wxWindowID id, cons
     auto statusMessageLabel = new wxStaticText(this, wxID_ANY, _("Message:"), wxDefaultPosition, wxDefaultSize);
     reportingSettingsSizer->Add(statusMessageLabel, 0, wxALL | wxALIGN_LEFT | wxALIGN_CENTER_VERTICAL, 5);
 
-    m_statusMessage = new wxComboBox(this, wxID_ANY, _(""), wxDefaultPosition, wxSize(125, -1), 0, nullptr, wxCB_DROPDOWN | wxTE_PROCESS_ENTER);
+    m_statusMessage = new wxComboBox(this, wxID_ANY, _(""), wxDefaultPosition, wxSize(175, -1), 0, nullptr, wxCB_DROPDOWN | wxTE_PROCESS_ENTER);
     reportingSettingsSizer->Add(m_statusMessage, 0, wxALL | wxALIGN_LEFT | wxALIGN_CENTER_VERTICAL, 5);
 
     m_buttonSet = new wxButton(this, wxID_ANY, _("Set"));
