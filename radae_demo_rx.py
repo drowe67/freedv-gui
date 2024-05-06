078e75e04ec3ddde8d01c86942c6d545f3103eb8
Mooneer Salem <mooneer@gmail.com>
2024-05-06 00:24:13 -0700

    Refactor 'vocoder' script to allow easier parameter adjustment.

--- radae_demo_rx.py
+++ radae_demo_rx.py
@@ -7,15 +7,16 @@ with open("/home/parallels/freedv-gui/rx_radae_demo.s16", "rb") as f:
     entire_file = f.read()
 
 ctr = 0
+num_to_read = 1024
 while True:
-    b = sys.stdin.buffer.read(160)
-    if len(b) < 160: break
+    b = sys.stdin.buffer.read(num_to_read)
+    if len(b) < num_to_read: break
 
-    sys.stderr.write("read block\n")
+    #sys.stderr.write("read block\n")
 
-    if (ctr + 160) > len(entire_file):
+    if (ctr + num_to_read) > len(entire_file):
         sys.stdout.buffer.write(entire_file[ctr:])
         ctr = 0
     else:
-        sys.stdout.buffer.write(entire_file[ctr:ctr + 160])
-        ctr = ctr + 160
+        sys.stdout.buffer.write(entire_file[ctr:ctr + num_to_read])
+        ctr = ctr + num_to_read
