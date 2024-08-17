dafa7af21f83e0add45b28413f29d1b2bb9845c5
Mooneer Salem <mooneer@gmail.com>
2024-08-17 16:45:04 -0700

    Figured out how to get valid audio into radae_tx.py.

--- radae_demo_tx.sh
+++ radae_demo_tx.sh
@@ -10,7 +10,7 @@ cd $RADAE_PATH
 # The below does the following:
 #     * For each block of OTA audio from freedv-gui:
 #         * Convert the audio into IQ data via zero-padding.
-#         * Pass the IQ data into the RADAE decoder 
-#         * Send the resulting 16 kHz audio back to freedv-gui.
+#         * Pass the IQ data into the RADAE encoder
+#         * Send the resulting 8 kHz audio back to freedv-gui.
 export PATH=$RADAE_VENV/bin:$PATH
-$RADAE_PATH/inference.sh $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth /dev/stdin /dev/null --end_of_over --auxdata --EbNodB 100 --bottleneck 3 --pilots --cp 0.004 --rate_Fs --write_rx /dev/stdout | python3 $RADAE_PATH/f32toint16.py --real --scale 16383
+$RADAE_PATH/build/src/lpcnet_demo -features /dev/stdin /dev/stdout | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_tx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | python3 $RADAE_PATH/f32toint16.py --real --scale 16383
