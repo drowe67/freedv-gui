4d22ce9d4c32943d16de2de2ba5f8d2d10d43305
Mooneer Salem <mooneer@gmail.com>
2024-08-17 15:59:24 -0700

    RADAE scripts need to execute from RADAE directory.

--- radae_demo_rx.sh
+++ radae_demo_rx.sh
@@ -3,9 +3,13 @@
 RADAE_PATH=$1
 RADAE_VENV=$2
 
+# Current RADAE scripts seem to require being executed from
+# the RADAE folder.
+cd $RADAE_PATH
+
 # The below does the following:
 #     * For each block of OTA audio from freedv-gui:
 #         * Convert the audio into IQ data via zero-padding.
 #         * Pass the IQ data into the RADAE decoder 
 #         * Send the resulting 16 kHz audio back to freedv-gui.
-$RADAE_VENV/bin/python3 $RADAE_PATH/int16tof32.py --zeropad | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_rx.py $RADAE_PATH/model17/checkpoints/checkpoint_epoch_100.pth -v 1 | $RADAE_PATH/build/src/lpcnet_demo -fargan-synthesis - -
+$RADAE_VENV/bin/python3 $RADAE_PATH/int16tof32.py --zeropad | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_rx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth -v 2 --auxdata | $RADAE_PATH/build/src/lpcnet_demo -fargan-synthesis - -
--- radae_demo_tx.sh
+++ radae_demo_tx.sh
@@ -3,6 +3,10 @@
 RADAE_PATH=$1
 RADAE_VENV=$2
 
+# Current RADAE scripts seem to require being executed from
+# the RADAE folder.
+cd $RADAE_PATH
+
 # The below does the following:
 #     * For each block of OTA audio from freedv-gui:
 #         * Convert the audio into IQ data via zero-padding.
