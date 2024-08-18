5a325b15af8d2ded22ba2507e31426f7d397efd1
Mooneer Salem <mooneer@gmail.com>
2024-08-18 16:50:35 -0700

    Cleanup TX/RX scripts and disable buffering.

--- radae_demo_rx.sh
+++ radae_demo_rx.sh
@@ -3,13 +3,16 @@
 RADAE_PATH=$1
 RADAE_VENV=$2
 
-# Current RADAE scripts seem to require being executed from
-# the RADAE folder.
-cd $RADAE_PATH
-
 # The below does the following:
 #     * For each block of OTA audio from freedv-gui:
-#         * Convert the audio into IQ data via zero-padding.
+#         * Convert the 8 kHz audio into IQ data via zero-padding.
 #         * Pass the IQ data into the RADAE decoder 
-#         * Send the resulting 16 kHz audio back to freedv-gui.
-$RADAE_VENV/bin/python3 $RADAE_PATH/int16tof32.py --zeropad | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_rx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth -v 2 --auxdata | $RADAE_PATH/build/src/lpcnet_demo -fargan-synthesis - -
+#         * Send the resulting 16 kHz audio back to freedv-gui for playback.
+#
+# Note: Current RADAE scripts seem to require being executed from
+# the RADAE folder.
+cd $RADAE_PATH
+export PATH=$RADAE_VENV/bin:$PATH
+stdbuf -i0 -o0 python3 int16tof32.py --zeropad | \
+stdbuf -i0 -o0 python3 radae_rx.py model19_check3/checkpoints/checkpoint_epoch_100.pth -v 2 --auxdata | \
+stdbuf -i0 -o0 build/src/lpcnet_demo -fargan-synthesis - -
--- radae_demo_tx.sh
+++ radae_demo_tx.sh
@@ -3,14 +3,16 @@
 RADAE_PATH=$1
 RADAE_VENV=$2
 
-# Current RADAE scripts seem to require being executed from
+# The below does the following:
+#     * For each block of microphone audio from freedv-gui:
+#         * Extract features from 16 kHz input audio.
+#         * Pass features into the RADAE encoder.
+#         * Send the resulting 8 kHz audio back to freedv-gui.
+#
+# Note: Current RADAE scripts seem to require being executed from
 # the RADAE folder.
 cd $RADAE_PATH
-
-# The below does the following:
-#     * For each block of OTA audio from freedv-gui:
-#         * Convert the audio into IQ data via zero-padding.
-#         * Pass the IQ data into the RADAE encoder
-#         * Send the resulting 8 kHz audio back to freedv-gui.
 export PATH=$RADAE_VENV/bin:$PATH
-$RADAE_PATH/build/src/lpcnet_demo -features - - | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_tx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | python3 $RADAE_PATH/f32toint16.py --real --scale 16383
+stdbuf -i0 -o0 build/src/lpcnet_demo -features - - | \
+stdbuf -i0 -o0 python3 radae_tx.py model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | \
+stdbuf -i0 -o0 python3 f32toint16.py --real --scale 16383
