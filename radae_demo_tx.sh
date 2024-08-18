169a4f3136dcd739f7b825284073023ce6c881e8
Mooneer Salem <mooneer@gmail.com>
2024-08-17 17:00:10 -0700

    We can just use - instead of /dev/stdin and /dev/stdout.

--- radae_demo_tx.sh
+++ radae_demo_tx.sh
@@ -13,4 +13,4 @@ cd $RADAE_PATH
 #         * Pass the IQ data into the RADAE encoder
 #         * Send the resulting 8 kHz audio back to freedv-gui.
 export PATH=$RADAE_VENV/bin:$PATH
-$RADAE_PATH/build/src/lpcnet_demo -features /dev/stdin /dev/stdout | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_tx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | python3 $RADAE_PATH/f32toint16.py --real --scale 16383
+$RADAE_PATH/build/src/lpcnet_demo -features - - | $RADAE_VENV/bin/python3 $RADAE_PATH/radae_tx.py $RADAE_PATH/model19_check3/checkpoints/checkpoint_epoch_100.pth --auxdata | python3 $RADAE_PATH/f32toint16.py --real --scale 16383
