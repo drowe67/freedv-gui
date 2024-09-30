5fdcbca8453a7e764e4f4d1d6c120422f277cdad
Mooneer Salem <mooneer@gmail.com>
2024-09-30 08:42:32 -0700

    Adjust pth file to allow pip to work.

--- cmake/BuildRADE.cmake
+++ cmake/BuildRADE.cmake
@@ -23,9 +23,11 @@ endif (NOT download_pip_POPULATED)
 
 # Per https://bnikolic.co.uk/blog/python/2022/03/14/python-embedwin.html
 # there are some tweaks we need to make to the embeddable package first
-# before FreeDV can use it.
+# before FreeDV can use it. Additionally, renaming python312._pth does not
+# allow pip to work; additional content should be added to this file instead
+# (see example at https://github.com/sergeyyurkov1/make_portable_python_env/blob/master/make_portable_python_env.bat).
 file(MAKE_DIRECTORY ${download_python3_SOURCE_DIR}/DLLs)
-file(RENAME ${download_python3_SOURCE_DIR}/python312._pth ${download_python3_SOURCE_DIR}/python312.pth)
+file(WRITE ${download_python3_SOURCE_DIR}/python312._pth "Lib/site-packages\r\npython312.zip\r\n.\r\n\r\n# Uncomment to run site.main() automatically\r\nimport site")
 
 # Tell NSIS to install Python and pip into the same folder freedv.exe is in.
 # This makes looking for python3.dll easier later.
