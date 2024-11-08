8b6e122abf508a62ee4d3db40ff43cc551deb5af
Mooneer Salem <mooneer@gmail.com>
2024-11-07 19:24:23 -0800

    Use script to build drivers instead.

---
+++ build_macos_sound_drivers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git clone https://github.com/tmiw/BlackHole.git
+cd BlackHole
+
+for i in {1..3}; do
+    export bundleID=audio.existential.BlackHole$i
+    export driverName=BlackHole$i
+    xcodebuild \
+        -project BlackHole.xcodeproj \
+        -configuration Release \
+        -target BlackHole \
+        CONFIGURATION_BUILD_DIR=build \
+        PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
+        GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS \
+            kNumber_Of_Channels='2' \
+            kPlugIn_BundleID=\"'$bundleID'\" \
+            kDriver_Name=\"'$driverName'\"' \
+        MACOSX_DEPLOYMENT_TARGET=10.13
+
+    sudo mv build/BlackHole.driver /Library/Audio/Plug-Ins/HAL/$driverName.driver
+done
+
+sudo killall -9 coreaudiod
