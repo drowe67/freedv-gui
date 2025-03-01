777485c627cf86b8f5cf84242feb55762c2757da
Mooneer Salem <mooneer@gmail.com>
2025-02-28 23:00:13 -0800

    Reduce websocketpp buffer size to match what's used in our read logic.

--- src/util/websocketpp_config.h
+++ src/util/websocketpp_config.h
@@ -195,7 +195,7 @@ struct custom_config {
         websocketpp::log::alevel::all ^ websocketpp::log::alevel::devel;
 
     ///
-    static const size_t connection_read_buffer_size = 16384;
+    static const size_t connection_read_buffer_size = 1024;
 
     /// Drop connections immediately on protocol error.
     /**
