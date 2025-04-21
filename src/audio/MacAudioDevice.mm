67835021e18a3b879e3508d055c5814a08fd490a
Mooneer Salem <mooneer@gmail.com>
2025-04-21 01:48:08 -0700

    Revert "macOS: try a smaller block size again."
    This reverts commit 1d21ad69344101326107c7d04904bdc5547b590e.

--- src/audio/MacAudioDevice.mm
+++ src/audio/MacAudioDevice.mm
@@ -39,7 +39,7 @@ thread_local void* MacAudioDevice::joinToken_ = nullptr;
 constexpr static double kOneNanosecond = 1.0e9;
 
 // The I/O interval time in seconds.
-constexpr static double AUDIO_SAMPLE_BLOCK_SEC = 0.005;
+constexpr static double AUDIO_SAMPLE_BLOCK_SEC = 0.010;
 
 static OSStatus GetIOBufferFrameSizeRange(AudioObjectID inDeviceID,
                                           UInt32* outMinimum,
