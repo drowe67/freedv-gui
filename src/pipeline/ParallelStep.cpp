51606a5ac19f168a143edef220fa55fd72dbcdf5
Mooneer Salem <mooneer@gmail.com>
2025-05-18 23:31:09 -0400

    Revert "Add logic to read in any data that may have come in during processing."
    This reverts commit 11ce99bb6a9ee2a357e3e15a207c09837f8c0a13.

--- src/pipeline/ParallelStep.cpp
+++ src/pipeline/ParallelStep.cpp
@@ -247,20 +247,16 @@ std::shared_ptr<short> ParallelStep::execute(std::shared_ptr<short> inputSamples
 
 void ParallelStep::executeRunnerThread_(ThreadInfo* threadState)
 {
-    int samplesIn = 0;
-    do
+    int samplesIn = codec2_fifo_used(threadState->inputFifo);
+    int samplesOut = 0;
+    if (codec2_fifo_read(threadState->inputFifo, threadState->tempInput.get(), samplesIn) != 0)
     {
-        samplesIn = codec2_fifo_used(threadState->inputFifo);
-        int samplesOut = 0;
-        if (codec2_fifo_read(threadState->inputFifo, threadState->tempInput.get(), samplesIn) != 0)
-        {
-            samplesIn = 0;
-        }
+        samplesIn = 0;
+    }
 
-        auto output = threadState->step->execute(threadState->tempInput, samplesIn, &samplesOut);
-        if (samplesOut > 0)
-        {
-            codec2_fifo_write(threadState->outputFifo, output.get(), samplesOut);
-        }
-    } while (codec2_fifo_used(threadState->inputFifo) > 0);
+    auto output = threadState->step->execute(threadState->tempInput, samplesIn, &samplesOut);
+    if (samplesOut > 0)
+    {
+        codec2_fifo_write(threadState->outputFifo, output.get(), samplesOut);
+    }
 }
