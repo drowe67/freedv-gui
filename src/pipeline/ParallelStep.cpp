21abe815c0f98ce3dd5ac43453460a85f1d3d99c
Mooneer Salem <mooneer@gmail.com>
2025-05-23 19:29:54 -0700

    Only limit output samples if input samples were provided.

--- src/pipeline/ParallelStep.cpp
+++ src/pipeline/ParallelStep.cpp
@@ -242,7 +242,14 @@ std::shared_ptr<short> ParallelStep::execute(std::shared_ptr<short> inputSamples
     assert(stepToOutput >= 0 && (size_t)stepToOutput < parallelSteps_.size());
     
     ThreadInfo* outputTask = threads_[stepToOutput];
-    *numOutputSamples = std::min(codec2_fifo_used(outputTask->outputFifo), numInputSamples * (outputSampleRate_ / inputSampleRate_));
+    if (numInputSamples > 0)
+    {
+        *numOutputSamples = std::min(codec2_fifo_used(outputTask->outputFifo), numInputSamples * (outputSampleRate_ / inputSampleRate_));
+    }
+    else
+    {
+        *numOutputSamples = codec2_fifo_used(outputTask->outputFifo);
+    }
     codec2_fifo_read(outputTask->outputFifo, outputTask->tempOutput.get(), *numOutputSamples);    
     return outputTask->tempOutput;
 }
