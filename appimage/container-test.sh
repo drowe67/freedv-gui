efda7bb21ab86d70f20dec00e0fc09bc05983043
Mooneer Salem <mooneer@gmail.com>
2025-06-19 17:15:38 -0700

    Forgot to start pipewire.

--- appimage/container-test.sh
+++ appimage/container-test.sh
@@ -8,6 +8,7 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR
 eval "$(dbus-launch --sh-syntax --exit-with-x11)"
+systemctl --user start pipewire.service pipewire-pulse.service wireplumber.service
 metacity --sm-disable --replace &
 sleep 5
 ./test/test_zeros.sh txrx RADEV1
