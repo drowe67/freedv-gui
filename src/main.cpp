077a7bf8051ef547634d195df56b4e1310c7a0b5
Mooneer Salem <mooneer@gmail.com>
2025-07-03 08:06:06 -0700

    Revert "Increase time again."
    This reverts commit 44073477ef91f0b66830907ed586ae53a53cfdcd.

--- src/main.cpp
+++ src/main.cpp
@@ -230,7 +230,7 @@ void MainApp::UnitTest_()
     }
     engine->stop();
 
-    std::this_thread::sleep_for(5s);
+    std::this_thread::sleep_for(1s);
 
     // Bring window to the front
     std::promise<void> prom1;
