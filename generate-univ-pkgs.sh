84f771b8953cde16726b167eca401ee178126295
Mooneer Salem <mooneer@gmail.com>
2025-07-24 00:54:48 -0700

    Use custom torch 2.7.1 for macOS to take advantage of performance improvements over 2.2.

--- generate-univ-pkgs.sh
+++ generate-univ-pkgs.sh
@@ -33,14 +33,15 @@ pip3 install delocate
 cp ../delocating.py.patched pkg-venv/lib/python3.12/site-packages/delocate/delocating.py # >0.10.7 has bugs with numpy, see https://github.com/matthew-brett/delocate/issues/229
 
 # Download RADE dependencies appropriate for both architectures (x86_64 and arm64)
-# Note: PyTorch 2.2.2 is the last one with binaries for both x86 and ARM.
 
 mkdir arm64
-pip3 download --platform macosx_11_0_arm64 --python-version 3.12 --only-binary=:all: numpy==1.26.4 torch==2.2.2 torchaudio==2.2.2 matplotlib --extra-index-url https://download.pytorch.org/whl/cpu
+pip3 download --platform macosx_11_0_arm64 --python-version 3.12 --only-binary=:all: filelock typing-extensions>=4.10.0 setuptools sympy>=1.13.3 networkx jinja2 fsspec optree>=0.13.0 opt-einsum>=3.3 numpy>=2 matplotlib
+wget -O arm64/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_arm64.whl https://k6aq.net/freedv-build/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_arm64.whl
 mv *arm64.whl arm64
 
 mkdir x86_64
-pip3 download --platform macosx_11_0_x86_64 --python-version 3.12 --only-binary=:all: numpy==1.26.4 torch==2.2.2 torchaudio==2.2.2 matplotlib --extra-index-url https://download.pytorch.org/whl/cpu
+pip3 download --platform macosx_11_0_x86_64 --python-version 3.12 --only-binary=:all: filelock typing-extensions>=4.10.0 setuptools sympy>=1.13.3 networkx jinja2 fsspec optree>=0.13.0 opt-einsum>=3.3 numpy>=2 matplotlib
+wget -O x86_64/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_x86_64.whl https://k6aq.net/freedv-build/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_x86_64.whl
 mv *x86_64.whl x86_64
 
 delete_pkg_if_universal2_downloaded "x86_64"
