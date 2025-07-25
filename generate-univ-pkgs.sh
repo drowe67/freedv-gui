69b8f25471ac38fc6dfa9b39fe1d31d2ac47a13c
Mooneer Salem <mooneer@gmail.com>
2025-07-25 16:55:55 -0700

    Fix issues with package generation script.

--- generate-univ-pkgs.sh
+++ generate-univ-pkgs.sh
@@ -29,18 +29,19 @@ cd pkg-tmp
 ../Python.framework/Versions/Current/bin/python3 -m venv pkg-venv
 . ./pkg-venv/bin/activate
 
+pip3 install --upgrade pip
 pip3 install delocate 
 cp ../delocating.py.patched pkg-venv/lib/python3.12/site-packages/delocate/delocating.py # >0.10.7 has bugs with numpy, see https://github.com/matthew-brett/delocate/issues/229
 
 # Download RADE dependencies appropriate for both architectures (x86_64 and arm64)
 
 mkdir arm64
-pip3 download --platform macosx_11_0_arm64 --python-version 3.12 --only-binary=:all: filelock typing-extensions>=4.10.0 setuptools sympy>=1.13.3 networkx jinja2 fsspec optree>=0.13.0 opt-einsum>=3.3 numpy>=2 matplotlib
+pip3 download --platform macosx_11_0_arm64 --python-version 3.12 --only-binary=:all: filelock typing-extensions setuptools sympy networkx jinja2 fsspec optree opt-einsum numpy matplotlib
 wget -O arm64/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_arm64.whl https://k6aq.net/freedv-build/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_arm64.whl
 mv *arm64.whl arm64
 
 mkdir x86_64
-pip3 download --platform macosx_11_0_x86_64 --python-version 3.12 --only-binary=:all: filelock typing-extensions>=4.10.0 setuptools sympy>=1.13.3 networkx jinja2 fsspec optree>=0.13.0 opt-einsum>=3.3 numpy>=2 matplotlib
+pip3 download --platform macosx_11_0_x86_64 --python-version 3.12 --only-binary=:all: filelock typing-extensions setuptools sympy networkx jinja2 fsspec optree opt-einsum numpy matplotlib
 wget -O x86_64/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_x86_64.whl https://k6aq.net/freedv-build/torch-2.7.1a0+gite2d141d-cp312-cp312-macosx_11_0_x86_64.whl
 mv *x86_64.whl x86_64
 
