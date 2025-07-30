9a946aaedff4b7b434a0bece21b39bc62232feb8
Mooneer Salem <mooneer@gmail.com>
2025-07-29 21:11:52 -0700

    Back to 60ms.

--- src/audio/MacAudioDevice.cpp
+++ src/audio/MacAudioDevice.cpp
@@ -681,7 +681,7 @@ void MacAudioDevice::setHelperRealTime()
     
     // Define constants determining how much time the audio thread can
     // use in a given time quantum.  All times are in milliseconds.
-    const double kTimeQuantum = 40; // 60ms, 1/2 of a RADEV1 block and confirmed to be sufficient with Instruments analysis.
+    const double kTimeQuantum = 60; // 60ms, 1/2 of a RADEV1 block and confirmed to be sufficient with Instruments analysis.
     
     // Time guaranteed each quantum.
     const double kAudioTimeNeeded = kGuaranteedAudioDutyCycle * kTimeQuantum;
