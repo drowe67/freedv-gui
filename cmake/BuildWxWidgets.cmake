f4ca4efdce1b0ec74fd0586f0504bbc98c4084ce
Mooneer Salem <mooneer@gmail.com>
2025-07-31 07:01:50 -0700

    Update Windows/macOS wxWidgets patch for 3.3.

--- cmake/BuildWxWidgets.cmake
+++ cmake/BuildWxWidgets.cmake
@@ -1,4 +1,4 @@
-set(WXWIDGETS_VERSION "3.2.8")
+set(WXWIDGETS_VERSION "3.3.1")
 
 # Ensure that the wxWidgets library is staticly built.
 set(wxBUILD_SHARED OFF CACHE BOOL "Build wx libraries as shared libs")
