afcf746462f6f48ea1d993c2c5fcc5546e9b096b
Mooneer Salem <mooneer@gmail.com>
2025-08-19 13:00:55 -0700

    Need to set config value and not the slider (middle-click).

--- src/ongui.cpp
+++ src/ongui.cpp
@@ -1413,5 +1413,14 @@ void MainFrame::updateReportingFreqList_()
 
 void MainFrame::OnResetMicSpkrLevel(wxMouseEvent& event)
 {
-    m_sliderMicSpkrLevel->SetValue(0);
+    bool txState = g_tx.load(std::memory_order_relaxed);
+
+    if (txState)
+    {
+        wxGetApp().appConfiguration.filterConfiguration.micInChannel.volInDB = 0;
+    }
+    else
+    {
+        wxGetApp().appConfiguration.filterConfiguration.spkOutChannel.volInDB = 0;
+    }
 }
