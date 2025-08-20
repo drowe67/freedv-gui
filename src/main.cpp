1120ee6e864a924b08f88f545e1fbcbde6e149aa
Mooneer Salem <mooneer@gmail.com>
2025-08-19 23:56:51 -0700

    Disable mic/speaker level slider unless modem is active.

--- src/main.cpp
+++ src/main.cpp
@@ -2364,6 +2364,8 @@ void MainFrame::performFreeDVOn_()
 
                 executeOnUiThreadAndWait_([&]() 
                 {
+                    m_sliderMicSpkrLevel->Enable(true);
+
         #ifdef _USE_TIMER
                     m_plotTimer.Start(_REFRESH_TIMER_PERIOD, wxTIMER_CONTINUOUS);
                     m_plotWaterfallTimer.Start(_REFRESH_TIMER_PERIOD, wxTIMER_CONTINUOUS);
@@ -2404,6 +2406,8 @@ void MainFrame::performFreeDVOff_()
 #ifdef _USE_TIMER
     executeOnUiThreadAndWait_([&]() 
     {
+        m_sliderMicSpkrLevel->Enable(false);
+
         m_plotTimer.Stop();
         m_plotWaterfallTimer.Stop();
         m_plotSpectrumTimer.Stop();
