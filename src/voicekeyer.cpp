49bfd2f509b51b6e26f67c6354d54e7cc320af1f
Mooneer Salem <mooneer@gmail.com>
2025-10-07 12:11:53 -0700

    Print error if one attempts to use a stereo WAV file instead of a mono one. (#1052)
    * Print error if one attempts to use a stereo WAV file instead of a mono one.
    
    * Add PR #1052 to changelog.

--- src/voicekeyer.cpp
+++ src/voicekeyer.cpp
@@ -231,7 +231,7 @@ int MainFrame::VoiceKeyerStartTx(void)
     else {
         g_sfTxFs = sfInfo.samplerate;
         
-        if (g_sfTxFs < 16000)
+        if (g_sfTxFs < freedvInterface.getRxSpeechSampleRate())
         {
             wxMessageBox(wxT("The selected voice keyer file does not have a high enough sample rate to guarantee acceptable audio quality. Please ensure that your file's sample rate is 16 kHz or greater."), wxT("Sample Rate Too Low"), wxOK);
             sf_close(tmpPlayFile);
@@ -239,7 +239,16 @@ int MainFrame::VoiceKeyerStartTx(void)
             m_togBtnVoiceKeyer->SetValue(false);
             return VK_IDLE;
         }
-        
+       
+        if (sfInfo.channels != 1)
+        {
+            wxMessageBox(wxT("The selected voice keyer file must only contain a single channel. Please use an audio editor to convert the file to a mono file."), wxT("Too Many Channels"), wxOK);
+            sf_close(tmpPlayFile);
+            m_togBtnVoiceKeyer->SetBackgroundColour(wxNullColour);
+            m_togBtnVoiceKeyer->SetValue(false);
+            return VK_IDLE;
+        }
+ 
         g_sfPlayFile = tmpPlayFile;
         
         SetStatusText(wxT("Voice Keyer: Playing file ") + wxString::FromUTF8(vkFileName_.c_str()) + wxT(" to mic input") , 0);
