4592c34d17217e187dba753d9a6582e74e478de1
Mooneer Salem <mooneer@gmail.com>
2025-10-22 08:38:49 -0700

    Fix compiler errors on Ubuntu 22.04.

--- src/util/GenericFIFO.h
+++ src/util/GenericFIFO.h
@@ -29,6 +29,15 @@
 #include <algorithm>
 #include <atomic>
 
+#ifdef __cpp_lib_hardware_interference_size
+    using std::hardware_constructive_interference_size;
+    using std::hardware_destructive_interference_size;
+#else
+    // 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
+    constexpr std::size_t hardware_constructive_interference_size = 64;
+    constexpr std::size_t hardware_destructive_interference_size = 64;
+#endif
+
 template<typename T>
 class GenericFIFO
 {
@@ -50,15 +59,15 @@ public:
 private:
     T* buf;
 
-#if !defined(__clang__)
+#if !defined(__clang__) && defined(__cpp_lib_hardware_interference_size)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Winterference-size"
 #endif // !defined(__clang__)
-    alignas(std::hardware_destructive_interference_size) std::atomic<T*> pin;
-    alignas(std::hardware_destructive_interference_size) std::atomic<T*> pout;
+    alignas(hardware_destructive_interference_size) std::atomic<T*> pin;
+    alignas(hardware_destructive_interference_size) std::atomic<T*> pout;
 #if !defined(__clang__)
 #pragma GCC diagnostic pop
-#endif // !defined(__clang__)
+#endif // !defined(__clang__) && defined(__cpp_lib_hardware_interference_size)
 
     int nelem;
     bool ownBuffer_;
