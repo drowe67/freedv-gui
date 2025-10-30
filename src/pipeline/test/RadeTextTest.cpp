281b69d5f299143e9d0b4e5f54c2b16d0304ff0b
Mooneer Salem <mooneer@gmail.com>
2025-10-29 18:00:44 -0700

    Fix compiler errors.

--- src/pipeline/test/RadeTextTest.cpp
+++ src/pipeline/test/RadeTextTest.cpp
@@ -24,9 +24,9 @@ void addNoise(short* ptr, int size)
     for (int i = 0; i < size; i++)
     {
         double noise = 16384 * distribution(generator);
-        noise = (noise + ptr[i]) / 0.707;
-        if (noise > INT_MAX) noise = INT_MAX;
-        else if (noise < INT_MIN) noise = INT_MIN;
+        noise = (noise + (double)ptr[i]) / 0.707;
+        if (noise > SHRT_MAX) noise = SHRT_MAX;
+        else if (noise < SHRT_MIN) noise = SHRT_MIN;
         ptr[i] = noise;
     }
 }
