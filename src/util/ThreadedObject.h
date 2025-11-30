9f069d42b90028dcf8a55d4dd8650dfb2a7f996b
Mooneer Salem <mooneer@gmail.com>
2025-11-29 18:24:22 -0800

    Fix compiler error.

--- src/util/ThreadedObject.h
+++ src/util/ThreadedObject.h
@@ -52,7 +52,7 @@ protected:
 private:
     ThreadedObject* parent_;
     std::string name_;
-    bool suppressEnqueue_;
+    std::atomic<bool> suppressEnqueue_;
 
 #if defined(__APPLE__)
     dispatch_queue_t queue_;
