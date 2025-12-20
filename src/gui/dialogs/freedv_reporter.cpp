fa4bf18b8e99b693554d98fabb150d46366243d7
Mooneer Salem <mooneer@gmail.com>
2025-12-20 01:06:37 -0800

    Revert Windows-specific changes.

--- src/gui/dialogs/freedv_reporter.cpp
+++ src/gui/dialogs/freedv_reporter.cpp
@@ -30,10 +30,6 @@
 #include <wx/uilocale.h>
 #endif // wxCHECK_VERSION(3,2,0)
 
-#if defined(WIN32)
-#include <wx/headerctrl.h>
-#endif // defined(WIN32)
-
 #include "freedv_reporter.h"
 #include "freedv_interface.h"
 
@@ -224,14 +220,8 @@ FreeDVReporterDialog::FreeDVReporterDialog(wxWindow* parent, wxWindowID id, cons
             wxGetApp().appConfiguration.reportingConfiguration.freedvReporterColumnVisibility->push_back(true);
         }
     }
-
-    // Windows seems to have the model column ID equal to the actual column ID regardless of the 
-    // actual ordering, so we just use wxHeaderCtrl to save/restore the column ordering.
-#if defined(WIN32)
-    for (auto col = 0; col < RIGHTMOST_COL; col++)
-#else    
+    
     for (auto& col : wxGetApp().appConfiguration.reportingConfiguration.freedvReporterColumnOrder.get())
-#endif // defined(WIN32)
     {
         if (col < RIGHTMOST_COL)
         {
@@ -248,19 +238,6 @@ FreeDVReporterDialog::FreeDVReporterDialog(wxWindow* parent, wxWindowID id, cons
     }
     m_listSpots->AppendTextColumn(wxT(" "), RIGHTMOST_COL, wxDATAVIEW_CELL_INERT, 1, wxALIGN_CENTER, wxDATAVIEW_COL_RESIZABLE);
 
-#if defined(WIN32)
-    auto headerCtrl = m_listSpots->GenericGetHeader();
-    wxArrayInt wxColumnOrder;
-    for (auto& col : wxGetApp().appConfiguration.reportingConfiguration.freedvReporterColumnOrder.get())
-    {
-        if (col < RIGHTMOST_COL)
-        {
-            wxColumnOrder.Add(col);
-        }
-    }
-    headerCtrl->SetColumnsOrder(wxColumnOrder);
-#endif // defined(WIN32)
-    
     sectionSizer->Add(m_listSpots, 0, wxALL | wxEXPAND, 2);
     
     // Bottom buttons
@@ -1220,37 +1197,20 @@ void FreeDVReporterDialog::SkipMouseEvent(wxMouseEvent&)
     OnItemRightClick(contextEvent);
 }
 
-#if defined(WIN32)
-void FreeDVReporterDialog::OnColumnReordered(wxDataViewEvent& event)
-#else
 void FreeDVReporterDialog::OnColumnReordered(wxDataViewEvent&)
-#endif // defined(WIN32)
 {
     // Preserve new column ordering
-    // Note: Windows uses the same indices for model column and GetColumn()
-    // so we need to use an alternate implementation for that platform.
     std::vector<int> newColPositions;
     std::stringstream ss;
-#if defined(WIN32)
-    log_info("column moved to %d", event.GetColumn());
-    auto headerCtrl = m_listSpots->GenericGetHeader();
-    wxArrayInt wxColumnOrder = headerCtrl->GetColumnsOrder();
-    for (unsigned int index = 0; index < wxColumnOrder.GetCount(); index++)
-    {
-        auto col = wxColumnOrder.Item(index);
-        newColPositions.push_back(col);
-        ss << col << " ";
-    }
-#else
     for (unsigned int index = 0; index < m_listSpots->GetColumnCount() - 1; index++)
     {
         auto dvc = m_listSpots->GetColumn(index);
         newColPositions.push_back(dvc->GetModelColumn());
+
         ss << dvc->GetModelColumn() << " ";
     }
-#endif // defined(WIN32)
-
     wxGetApp().appConfiguration.reportingConfiguration.freedvReporterColumnOrder = newColPositions;
+
     log_info("New column ordering: %s", ss.str().c_str());
 }
 
