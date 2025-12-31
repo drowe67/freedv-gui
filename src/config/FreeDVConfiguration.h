0d0c069d48ed025e5dbbcedd9e444f71cc1b25a8
Mooneer Salem <mooneer@gmail.com>
2025-12-31 00:41:53 -0800

    Remove Quick Record options as they are no longer necessary.

--- src/config/FreeDVConfiguration.h
+++ src/config/FreeDVConfiguration.h
@@ -87,9 +87,7 @@ public:
     ConfigurationDataElement<bool> halfDuplexMode;
     ConfigurationDataElement<bool> multipleReceiveEnabled;
     ConfigurationDataElement<bool> multipleReceiveOnSingleThread;
-    
-    ConfigurationDataElement<wxString> quickRecordPath;
-    
+        
     ConfigurationDataElement<bool> freedv700Clip;
     ConfigurationDataElement<bool> freedv700TxBPF;
     
