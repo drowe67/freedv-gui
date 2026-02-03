9781267403e20e5f3933cdc84e809c3cfa620916
Mooneer Salem <mooneer@gmail.com>
2026-02-03 07:53:33 -0800

    Disable setting pipewire mainloop affinity.

--- src/audio/PulseAudioEngine.cpp
+++ src/audio/PulseAudioEngine.cpp
@@ -82,6 +82,7 @@ void PulseAudioEngine::start()
 
         if (pa_threaded_mainloop_in_thread(threadedML))
         {
+#if 0
             auto numCpusAvailable = get_nprocs();
             if (numCpusAvailable > 2)
             {
@@ -94,6 +95,7 @@ void PulseAudioEngine::start()
                     log_warn("Could not pin thread to CPU %d and %d (errno = %d)", numCpusAvailable - 2, numCpusAvailable - 1, errno);
                 }
             }
+#endif //0
 
 #if defined(USE_RTKIT)
             DBusError error;
