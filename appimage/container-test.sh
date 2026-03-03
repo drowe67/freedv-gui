c05264e5f0d34c546edd41ed5516ca991de7aff7
Mooneer Salem <mooneer@gmail.com>
2026-03-02 17:08:24 -0800

    Set XDG_RUNTIME_DIR before Xvfb.

--- appimage/container-test.sh
+++ appimage/container-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /workspace
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
 Xvfb :99 -screen 0 1024x768x16 &
 sleep 5
 export DISPLAY=:99.0
-export XDG_RUNTIME_DIR=/run/user/$(id -u)
 mkdir -p $XDG_RUNTIME_DIR
 chmod 700 $XDG_RUNTIME_DIR
 eval "$(dbus-launch --sh-syntax --exit-with-x11)"
