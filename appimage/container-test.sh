dea9074216271d3a82b6b8e5456ee735c03a4844
Mooneer Salem <mooneer@gmail.com>
2026-03-03 01:30:43 -0800

    Add some additional environment from normal pipewire install.

--- appimage/container-test.sh
+++ appimage/container-test.sh
@@ -2,11 +2,15 @@
 
 cd /workspace
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export GIO_USE_VFS=local
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
 Xvfb :99 -screen 0 1024x768x16 &
 sleep 5
 export DISPLAY=:99.0
-mkdir -p $XDG_RUNTIME_DIR
-chmod 700 $XDG_RUNTIME_DIR
 eval "$(dbus-launch --sh-syntax --exit-with-x11)"
 sudo systemctl restart polkit
 sudo systemctl enable rtkit-daemon
