46321e033ba7ad0cf7d0c92e9700a118f6fa6e50
Mooneer Salem <mooneer@gmail.com>
2026-03-29 14:38:41 -0700

    More debugging.

--- appimage/container-test.sh
+++ appimage/container-test.sh
@@ -9,10 +9,11 @@ export GIO_USE_VFS=local
 Xvfb :99 -screen 0 1024x768x16 &
 sleep 5
 export DISPLAY=:99.0
-eval "$(dbus-launch --sh-syntax --exit-with-x11)"
-env
+#eval "$(dbus-launch --sh-syntax --exit-with-x11)"
 ls -la $XDG_RUNTIME_DIR
 systemd-run --user --scope --shell
+ls -la $XDG_RUNTIME_DIR
+env
 systemctl --user enable --now pipewire-pulse.socket
 systemctl --user enable --now pipewire.service
 systemctl --user enable --now wireplumber.service
