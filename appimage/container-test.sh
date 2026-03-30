619c67e8d26fccfb2b0a3b0cddf54d296d2b1a58
Mooneer Salem <mooneer@gmail.com>
2026-03-29 21:52:16 -0700

    Fix intermittent PipeWire related timeouts in GH environment. (#1272)
    * Fix intermittent PipeWire related timeouts in GH environment.
    
    * Add debugging info.
    
    * Try starting user service to see if we get the correct files in /run.
    
    * Minimal logic to make sure systemd is available as a user.
    
    * Additional debugging.
    
    * Try disabling SELinux.
    
    * More debugging.
    
    * Use id -un instead of hardcoding usernames.
    
    * Restart systemd-logind to see if that makes a difference.
    
    * Disable PGO to allow faster failures.
    
    * Oops, commented out something I shouldn't have.
    
    * Need groupadd command before useradd command.
    
    * Set up tmate session for further debugging.
    
    * Disable AppArmor on Ubuntu host.
    
    * oops, fix typo.
    
    * Reenable PGO.
    
    * Clean up PR prior to merge.

--- appimage/container-test.sh
+++ appimage/container-test.sh
@@ -9,10 +9,10 @@ export GIO_USE_VFS=local
 Xvfb :99 -screen 0 1024x768x16 &
 sleep 5
 export DISPLAY=:99.0
-eval "$(dbus-launch --sh-syntax --exit-with-x11)"
-pipewire &
-pipewire-pulse &
-wireplumber &
+systemd-run --user --scope --shell
+systemctl --user enable --now pipewire-pulse.socket
+systemctl --user enable --now pipewire.service
+systemctl --user enable --now wireplumber.service
 metacity --sm-disable --replace &
 sleep 5
 ./test/test_rade_loss.sh
