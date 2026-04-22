8265e0899127a2bf488bc0816b1bd9bae219aaea
Mooneer Salem <mooneer@gmail.com>
2026-04-21 23:34:07 -0700

    Try installing audio drivers as administrator.

---
+++ ci/Install-Driver.ps1
@@ -0,0 +1,15 @@
+# Make sure we are running as Admin
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+    }
+
+$certName = $args[0]
+$infName = $args[1]
+$hwId = $args[2]
+
+Import-Certificate -FilePath $certName -CertStoreLocation Cert:\LocalMachine\root
+Import-Certificate -FilePath $certNamer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+& "$Env:WindowsSdkDir\Tool\$Env:WindowsSDKVersion\$Env:Platform\devgen.exe" /add /bus ROOT /hardwareid $hwId
+pnputil /add-driver $infName /install
