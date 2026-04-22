872397309c8ff6d213edd4f523e4a8d4e068a9ff
Mooneer Salem <mooneer@gmail.com>
2026-04-22 11:08:02 -0700

    Update driver install script to create missing Registry key.

--- ci/Install-Driver.ps1
+++ ci/Install-Driver.ps1
@@ -10,6 +10,17 @@ $infName = $args[1]
 $hwId = $args[2]
 
 Import-Certificate -FilePath $certName -CertStoreLocation Cert:\LocalMachine\root
+
+# Some runners in the GH environment will emit "Access Denied" for Import-Certificate
+# if the proper TrustedPublisher Registry key doesn't exist first. Create prior to 
+# attempting import.
+# See https://learn.microsoft.com/en-us/answers/questions/1679945/(import-certificate)-unauthorizedaccessexception-e
+$Key = "HKLM:\Software\Microsoft\SystemCertificates\TrustedPublisher"
+If (-Not (Test-Path $Key)) {
+    New-Item -Path $Key -ItemType RegistryKey -Force
+}
+`
 Import-Certificate -FilePath $certName -CertStoreLocation Cert:\LocalMachine\TrustedPublisher
+
 & "$Env:WindowsSdkDir\Tools\$Env:WindowsSDKVersion\$Env:Platform\devgen.exe" /add /bus ROOT /hardwareid $hwId
 pnputil /add-driver $infName /install
