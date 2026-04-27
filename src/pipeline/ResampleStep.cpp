1ebd1349846b2a361422d7ce868a77d498271a5d
Mooneer Salem <mooneer@gmail.com>
2026-04-27 09:13:08 -0700

    Try lower transition band.

--- src/pipeline/ResampleStep.cpp
+++ src/pipeline/ResampleStep.cpp
@@ -48,7 +48,7 @@ ResampleStep::ResampleStep(int inputSampleRate, int outputSampleRate, bool)
 
     // r8brain is fast enough that we don't need special transition bands
     // for plots.
-    double reqTransBand = 9.0;
+    double reqTransBand = 5.0;
 
     resampleState_ = new r8b::CDSPResampler24(
         inputSampleRate, outputSampleRate, maxInputLen, reqTransBand);
