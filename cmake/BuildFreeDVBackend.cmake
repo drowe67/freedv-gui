f396c68763877ec2960ff7b2d2afb2cf33b21122
Mooneer Salem <mooneer@gmail.com>
2026-06-18 16:03:39 -0700

    Revert back to freedv-backend main.

--- cmake/BuildFreeDVBackend.cmake
+++ cmake/BuildFreeDVBackend.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     freedv_backend
     GIT_REPOSITORY https://github.com/tmiw/freedv-backend
-    GIT_TAG ms-eoo-refactor-end-silence
+    GIT_TAG main
 )
 
 FetchContent_MakeAvailable(freedv_backend)
