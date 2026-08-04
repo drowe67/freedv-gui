3501967719f479fd0d8c4994d69e66e07b0ed62e
Mooneer Salem <mooneer@gmail.com>
2026-08-04 13:29:46 -0700

    fullduplex_* should use actual audio, not a sine wave.

--- test/test_zeros.sh
+++ test/test_zeros.sh
@@ -57,11 +57,17 @@ if [ "$FREEDV_TEST" == "txrx" ]; then
     FREEDV_CONF_FILE=freedv-ctest-fullduplex.conf
     REC_DEVICE="$FREEDV_COMPUTER_TO_SPEAKER_DEVICE"
 
-    # Generate sine wave for input
+    # Generate input audio
+    if [ ! -d "$(pwd)/rade_src" ]; then
+        git clone -b main https://github.com/drowe67/radae.git rade_src
+    fi
+    sox $(pwd)/rade_src/wav/all.wav $(pwd)/rade_src/wav/all.wav -r 48000 $(pwd)/tx_in.wav
+
+    # Play input audio
     if [ "$OPERATING_SYSTEM" == "Linux" ]; then
-        sox -r 48000 -n -b 16 -c 1 -t wav - synth 120 sin 1000 vol -10dB | paplay -d "$FREEDV_MICROPHONE_TO_COMPUTER_DEVICE" &
+	paplay -d "$FREEDV_MICROPHONE_TO_COMPUTER_DEVICE" $(pwd)/tx_in.wav &
     else
-        sox --buffer 32768 -r 48000 -n -b 16 -c 1 -t $SOX_DRIVER "$FREEDV_MICROPHONE_TO_COMPUTER_DEVICE" - synth 120 sin 1000 vol -10dB &
+        sox -q --buffer 32768 $(pwd)/tx_in.wav -t $SOX_DRIVER "$FREEDV_MICROPHONE_TO_COMPUTER_DEVICE" &
     fi
     PLAY_PID=$!
 elif [ "$FREEDV_TEST" == "rx" ]; then
@@ -69,7 +75,7 @@ elif [ "$FREEDV_TEST" == "rx" ]; then
     if [ "$OPERATING_SYSTEM" == "Linux" ]; then
         paplay -d "$FREEDV_RADIO_TO_COMPUTER_DEVICE" $FREEDV_RX_FILE &
     else
-        sox --buffer 32768 $FREEDV_RX_FILE -t $SOX_DRIVER "$FREEDV_RADIO_TO_COMPUTER_DEVICE" &
+        sox -q --buffer 32768 $FREEDV_RX_FILE -t $SOX_DRIVER "$FREEDV_RADIO_TO_COMPUTER_DEVICE" &
     fi
     PLAY_PID=$!
     REC_DEVICE="$FREEDV_COMPUTER_TO_SPEAKER_DEVICE.monitor"
