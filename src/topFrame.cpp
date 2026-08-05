49ed8102c2e43c682049bb5957ca44c96c9ba704
Mooneer Salem <mooneer@gmail.com>
2026-08-05 10:31:46 -0700

    Fix compiler error.

--- src/topFrame.cpp
+++ src/topFrame.cpp
@@ -1050,7 +1050,7 @@ TopFrame::TopFrame(wxWindow* parent, wxWindowID id, const wxString& title, const
     m_freqBox->GetContentSizer()->Add(txtReportFreqSizer, 1, static_cast<int>(wxEXPAND), 1);
     //m_freqBox->GetContentSizer()->Add(reportFrequencyUnits, 0, wxALIGN_CENTER_VERTICAL, 1);
 
-    rightSizer->Add(reportFrequencySizer, 0, static_cast<int>(wxALL) | static_cast<int>(wxEXPAND), 2);
+    rightSizer->Add(txtReportFreqSizer, 0, static_cast<int>(wxALL) | static_cast<int>(wxEXPAND), 2);
     
     /* new --- */
 
