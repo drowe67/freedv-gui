fb0f3fa8111d38897365cb413e92d205c864d46a
Mooneer Salem <mooneer@gmail.com>
2026-08-24 01:07:26 -0700

    Release version 2.4.0.

--- cmake/BuildFreeDVBackend.cmake
+++ cmake/BuildFreeDVBackend.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     freedv_backend
     GIT_REPOSITORY https://github.com/tmiw/freedv-backend
-    GIT_TAG main
+    GIT_TAG v1.0.0
 )
 
 FetchContent_MakeAvailable(freedv_backend)
