cf4a3153f93b2c418ad9b8d4cfb65d1029c74aba
Mooneer Salem <mooneer@gmail.com>
2026-09-10 16:46:06 -0700

    Update rpath clear script to handle multiple different dupe rpaths.

--- src/clean-duplicate-rpaths.sh
+++ src/clean-duplicate-rpaths.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for i in `otool -l $1 | grep -A2 LC_RPATH | grep path | awk '{ print $2; }' | tail +2`; do
-    install_name_tool -delete_rpath "$i" $1
+RPATHS=`otool -l $1 | grep -A2 LC_RPATH | grep path | awk '{ print $2; }' | sort | uniq`
+for i in $RPATHS; do
+    for j in `otool -l $1 | grep -A2 LC_RPATH | grep path | awk '{ print $2; }' | grep $i | tail +2`; do
+        install_name_tool -delete_rpath "$j" $1
+    done
 done
