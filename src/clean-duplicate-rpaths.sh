cbd77005cfac95226ea3e4d7345684f396e18ee5
Mooneer Salem <mooneer@gmail.com>
2026-09-10 17:12:27 -0700

    Add debugging.

--- src/clean-duplicate-rpaths.sh
+++ src/clean-duplicate-rpaths.sh
@@ -3,6 +3,7 @@
 RPATHS=`otool -l $1 | grep -A2 LC_RPATH | grep path | awk '{ print $2; }' | sort | uniq`
 for i in $RPATHS; do
     for j in `otool -l $1 | grep -A2 LC_RPATH | grep path | awk '{ print $2; }' | grep $i | tail +2`; do
+        echo "Removing RPATH $j"
         install_name_tool -delete_rpath "$j" $1
     done
 done
